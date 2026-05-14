3d8602f56ae77a1d9d2d4941ac89dba7bd4ce1d3
CameronAron <cameron.aron04@gmail.com>
2026-05-14 18:46:08 -0500

    Imported slingshot mod files

---
+++ tools/slingshot_mod/src/mod.cpp
@@ -0,0 +1,93 @@
+#include "dusk/hook.hpp"
+#include "dusk/mod_api.h"
+#include "d/actor/d_a_alink.h"
+#include <cstdio>
+#include "m_Do/m_Do_audio.h"
+
+static DuskElemHandle g_el_slingshot = nullptr;
+static int g_shot_count = 0;
+static DuskElemHandle g_el_shots = nullptr;
+static int g_prev_shot_state = 0;
+
+static void on_checkUpperItemActionBow_post(void* args, void* retval) {
+    (void)args;
+    (void)retval;
+
+    daAlink_c* link = daAlink_getAlinkActorClass();
+
+    if (!link) {
+        return;
+    }
+
+    if (link->mEquipItem != dItemNo_PACHINKO_e) {
+        g_prev_shot_state = 0;
+        return;
+    }
+
+    int current = link->mItemVar0.field_0x3018;
+
+    if (g_prev_shot_state == 0 && current == 1) {
+        g_shot_count++;
+
+        dusk::g_api->log_info(
+            "Slingshot shot detected! Total shots: %d",
+            g_shot_count);
+    }
+
+    g_prev_shot_state = current;
+}
+
+static void BuildPanel(DuskPanelHandle panel, void*) {
+    dusk::g_api->panel_add_section(panel, "Status");
+
+    g_el_slingshot =
+        dusk::g_api->panel_add_dyn_text(panel, "Slingshot equipped: UNKNOWN");
+
+    g_el_shots =
+        dusk::g_api->panel_add_dyn_text(panel, "Shots fired: 0");
+}
+
+static void UpdatePanel(void*) {
+    daAlink_c* link = daAlink_getAlinkActorClass();
+
+    if (!link) {
+        dusk::g_api->elem_set_text(
+            g_el_slingshot,
+            "Slingshot equipped: NO PLAYER");
+        return;
+    }
+
+    bool equipped = link->mEquipItem == dItemNo_PACHINKO_e;
+
+    dusk::g_api->elem_set_text(
+        g_el_slingshot,
+        equipped ?
+        "Slingshot equipped: YES" :
+        "Slingshot equipped: NO");
+
+    char buf[64];
+    snprintf(buf, sizeof(buf), "Shots fired: %d", g_shot_count);
+
+    dusk::g_api->elem_set_text(g_el_shots, buf);
+}
+
+extern "C" {
+
+void mod_init(DuskModAPI* api) {
+    dusk::init(api);
+
+    api->register_tab_content(BuildPanel, nullptr);
+    api->register_tab_update(UpdatePanel, nullptr);
+
+    dusk::hookAddPost<&daAlink_c::checkUpperItemActionBow>(on_checkUpperItemActionBow_post);
+}
+
+void mod_tick(DuskModAPI* api) {
+    (void)api;
+}
+
+void mod_cleanup(DuskModAPI* api) {
+    (void)api;
+}
+
+}
